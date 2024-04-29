8e3aa1dd1513731d7b3e8ee77a41792f9b2f24fb
dorian2024 <157504892+dorian2024@users.noreply.github.com>
2024-04-29 21:36:42 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+﻿#include <SFML/Graphics.hpp>
+//#include <ctime>
+//using namespace sf;
+//using namespace std;
+//const int xResolution = 1900;
+//const int yResolution = 1000;
+//const int boxPixels = 50;
+//const int grassPixelsX = 250;
+//const int xRows = xResolution / boxPixels;
+//const int yRows = yResolution / boxPixels;
+//int gameRows[xRows][yRows] = {};
+//struct coordinates {
+//	int x;
+//	int y;
+//};
+//int main()
+//{
+//	return 0;
+//}
